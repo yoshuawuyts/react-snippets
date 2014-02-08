779791e96ae56b766b3c3507f2bf91271ef05b4f
yoshuawuyts <i@yoshuawuyts.com>
2014-02-08 23:56:09 +0100

    Add component snippet

---
+++ snippets/component.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+/**
+ * View
+ */
+
+exports.component = React.createClass({
+  displayName: 'componentName',
+
+  render: function() {
+    return (
+
+    );
+  }
+});
